70e53e4636f524071953ad252c3dc69106ef3bbd
Sergey Romanenko <pokeroklife@gmail.com>
2019-03-22 10:08:49 +0200

    work good

--- models/User.php
+++ models/User.php
@@ -35,7 +35,7 @@ class User extends ActiveRecord implements IdentityInterface
     public function rules()
     {
         return [
-            ['status', 'news', 'value' => self::STATUS_ACTIVE],
+            ['status', 'default', 'value' => self::STATUS_ACTIVE],
             ['status', 'in', 'range' => [self::STATUS_ACTIVE, self::STATUS_DELETED]],
         ];
     }
